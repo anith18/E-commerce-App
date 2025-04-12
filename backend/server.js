0e6c07adff639356a5044b5b811e5e8cb5018257
anith18 <anithraju174@gmail.com>
2025-04-12 12:22:10 +0530

    latest commit

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
-import productModel from './models/ProductModel.js';
+import productModel from './models/productModel.js';
 import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
